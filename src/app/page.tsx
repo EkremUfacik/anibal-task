08ffd8d54ac839de8611a5dcf93dff5b90a591e5
ekrem <ekremufacik07@gmail.com>
2023-11-04 15:08:19 +0300

    add spline animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ const Home = () => {
       <Header />
       <SectionOur />
       <Carousel />
+      <iframe
+        src="https://my.spline.design/clonermotioninfraredcopy-4b05346df83a063715be49220efad9f9/"
+        className="w-full h-screen mt-20"
+      ></iframe>
     </div>
   );
 };
